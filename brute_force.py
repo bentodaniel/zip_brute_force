e524a186517d4e531518843a17f79ed563f1bc39
Daniel <danielbento_98@hotmail.com>
2020-12-14 00:33:30 +0000

    no multiprocessing. back to basic brute force

--- brute_force.py
+++ brute_force.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from itertools import chain, product
 import string
 import time
-import multiprocessing as mp
-from functools import partial
 
 # Asks the user the name of the file until it is a valid file
 def get_file():
@@ -52,68 +50,12 @@ def get_charset():
     elif charset_type == 5:
         return "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()[]{+-}"
         
-
-def get_charset_combinations(charset, maxlength):
+"""
+def get_charset_combinations(charset, minlength, maxlength):
     return (''.join(candidate)
         for candidate in chain.from_iterable(product(charset, repeat=i)
-        for i in range(1, maxlength + 1)))
-
-def check_password(pw):
-    try:
-        zf.extractall(pwd=bytes(pw,'utf-8'))
-        print ("The password is " + pw)
-        return pw
-    except:
-        pass
-
-# generator is ran by the individual processes
-def generator(first_char, wordtail_length, length):
-
-    # Generate all strings of length `length` starting with `first_char`.
-    # Once length > wordtail_length, only the wordtail_length characters
-    # need to be generated.
-    if length <= wordtail_length:
-        assert first_char == ""
-        # creating all combinations of length charset here (short strings)
-        for t in product(charset, repeat=length):
-            result = "".join(t)
-            print(result)
-            # To compare to hash or keyword, do something with result here
-            check_password(result)
-    else:
-        assert len(first_char) + wordtail_length == length
-        # creating all combinations of length charset here (longer strings)
-        for t in product(charset, repeat=wordtail_length):
-            result = first_char + "".join(t)
-            print(result)
-            # To compare to hash or keyword, do something with result here
-            check_password(result)
-    
-# Bruteforce algorithm. Tests combination possibilities
-# of characters in charset
-def bruteforce(pool, chunk_size=1000000):
-    charset_length = len(charset)
-
-    # This chunk sets max largest wordtail_length to be
-    # computed without separating jobs by first_char
-    wordtail_length = 1
-    while charset_length**wordtail_length <= chunk_size:
-        wordtail_length += 1
-    wordtail_length -= 1
-
-    # short strings: generate words from size 1 to short words max
-    max_short_len = min(wordtail_length, pw_max_length)
-    for length in range(1, max_short_len + 1):
-        pool.apply_async(generator, args=("", wordtail_length, length))
-
-    # longer strings: generate words from short words max to maxlength
-    for length in range(max_short_len + 1, pw_max_length + 1):
-        # Get the cartesian product chars in the charset
-        for t in product(charset, repeat=length-wordtail_length):
-            first_char = "".join(t) # Remove punctuation
-
-            # Run generator function for on each process with args
-            pool.apply_async(generator, args=(first_char, wordtail_length, length))
+        for i in range(minlength, maxlength)))
+"""
 
 
 if __name__ == '__main__':
@@ -121,29 +63,22 @@ if __name__ == '__main__':
     zf = ZipFile(zip_file)
     pw_max_length = get_max_pw_size()
     charset = get_charset()
-    cores_used = None # defaults to all available cores
-    result_pw = ""
 
     print("\nCharacter set chosen: ", charset)
-
-    print("\nCores on machine to be used: ", mp.cpu_count())
 
     print("\n...Starting the Process....\n")
 
     start_time = time.time()
 
-    # Multiprocessing setup here
-    #pool = mp.Pool(cores_used)
-    #bruteforce(pool)
-    #pool.close()
-    #pool.join()
-
-    """
-    for attempt in product(charset, repeat=pw_max_length):
-        pw = "".join(attempt)
-        if check_password(pw) != None:
-            break
-    """
+    with ZipFile(zip_file) as zf:
+        for attempt in product(charset, repeat=pw_max_length):
+            pw = "".join(attempt)
+            try:
+                zf.extractall(pwd=bytes(pw,'utf-8'))
+                print ("The password is " + pw)
+                break
+            except:
+                pass
 
     elapsed_time = time.time() - start_time
     print("Elapsed time: {}".format(elapsed_time), "\n")
